8d369c6d0c5974801591b4eb8df69691c75c4170
Ztrolix <xdpxigaming@gmail.com>
2024-11-02 15:59:25 +1100

    Renamed 'ModMenuIntegration' -> 'modMenuIntegration'

--- src/client/java/dev/xdpxi/xdlib/config/modMenuIntegration.java
+++ src/client/java/dev/xdpxi/xdlib/config/modMenuIntegration.java
@@ -8,7 +8,7 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 
 @Environment(EnvType.CLIENT)
-public class ModMenuIntegration implements ModMenuApi {
+public class modMenuIntegration implements ModMenuApi {
     private static final boolean clothConfig = loader.isModLoaded("cloth-config");
     public static final configManager CONFIG_MANAGER = new configManager();
 
