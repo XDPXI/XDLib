a53d3ff4fbaa18689968044f2053759bc2ae5e2b
Ztrolix <xdpxigaming@gmail.com>
2024-11-19 17:43:19 +1100

    Removed changelogConfig
    Removed changelogConfig because the changelog screen is no longer showing anymore so there is no point of having the code.

--- src/client/java/dev/xdpxi/xdlib/mixin/client/TitleScreenMixin.java
+++ src/client/java/dev/xdpxi/xdlib/mixin/client/TitleScreenMixin.java
@@ -1,7 +1,5 @@
 package dev.xdpxi.xdlib.mixin.client;
 
-import dev.xdpxi.xdlib.config.changelogConfig;
-import dev.xdpxi.xdlib.config.configManager;
 import dev.xdpxi.xdlib.support;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
@@ -20,36 +18,22 @@ public class TitleScreenMixin {
     @Unique
     private static boolean Shown = false;
 
-    @Unique
-    private static int version = 12;
-
     @Inject(method = "init", at = @At("TAIL"))
     public void onInit(CallbackInfo ci) {
-        changelogConfig config = new changelogConfig();
-        changelogConfig.ConfigData loadedData = config.read();
-        int configVersion = loadedData.getVersion();
-        boolean firstStartup = version != configVersion;
-        boolean changelogEveryStartup = configManager.configData.isChangelogEveryStartup();
-        boolean disableChangelog = configManager.configData.isDisableChangelog();
-
-        if (((firstStartup || changelogEveryStartup) && !Shown) && !disableChangelog) {
-            // do nothing
-        } else {
-            if (!Shown) {
-                CompletableFuture.supplyAsync(() -> {
-                    try {
-                        Thread.sleep(2000);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                    return 0;
-                }).thenAcceptAsync(result -> {
-                    Shown = true;
-                    MinecraftClient client = MinecraftClient.getInstance();
-                    Screen currentScreen = client.currentScreen;
-                    client.execute(() -> client.setScreen(new support(Text.empty(), currentScreen)));
-                }, MinecraftClient.getInstance());
-            }
+        if (!Shown) {
+            CompletableFuture.supplyAsync(() -> {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                return 0;
+            }).thenAcceptAsync(result -> {
+                Shown = true;
+                MinecraftClient client = MinecraftClient.getInstance();
+                Screen currentScreen = client.currentScreen;
+                client.execute(() -> client.setScreen(new support(Text.empty(), currentScreen)));
+            }, MinecraftClient.getInstance());
         }
     }
 }
