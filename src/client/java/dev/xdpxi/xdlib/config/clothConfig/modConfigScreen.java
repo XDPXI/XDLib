c4f86acd04e20beffd5c8fa74bb9657f9aef81e8
Ztrolix <xdpxigaming@gmail.com>
2024-11-22 19:49:46 +1100

    Renamed getModConfigScreen -> modConfigScreen

---
+++ src/client/java/dev/xdpxi/xdlib/config/clothConfig/modConfigScreen.java
@@ -0,0 +1,33 @@
+package dev.xdpxi.xdlib.config.clothConfig;
+
+import dev.xdpxi.xdlib.config.configManager;
+import me.shedaniel.clothconfig2.api.ConfigBuilder;
+import me.shedaniel.clothconfig2.gui.entries.BooleanListEntry;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.text.Text;
+
+import java.util.function.Consumer;
+
+public class modConfigScreen {
+    public Screen getModConfigScreen(Screen parent) {
+        configManager.ConfigData configData = configManager.configData;
+
+        ConfigBuilder builder = ConfigBuilder.create()
+                .setParentScreen(parent)
+                .setTitle(Text.of("XDLib Configuration"));
+
+        builder.getOrCreateCategory(Text.of("Main"))
+                .addEntry(createBooleanToggle(builder, "Enable Custom Modmenu Badges", configData.isCustomBadges(), configData::setCustomBadges, true))
+                .addEntry(createBooleanToggle(builder, "Disable Title Screen Warnings", configData.isDisableTitlePopups(), configData::setDisableTitlePopups, false));
+
+        return builder.build();
+    }
+
+    private BooleanListEntry createBooleanToggle(ConfigBuilder builder, String text, boolean initialValue, Consumer<Boolean> consumer, boolean defaultValue) {
+        return builder.entryBuilder()
+                .startBooleanToggle(Text.of(text), initialValue)
+                .setDefaultValue(defaultValue)
+                .setSaveConsumer(consumer)
+                .build();
+    }
+}
--- src/client/java/dev/xdpxi/xdlib/config/modMenuIntegration.java
+++ src/client/java/dev/xdpxi/xdlib/config/modMenuIntegration.java
@@ -3,19 +3,18 @@ package dev.xdpxi.xdlib.config;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
 import dev.xdpxi.xdlib.api.mod.loader;
-import dev.xdpxi.xdlib.config.clothConfig.getModConfigScreen;
+import dev.xdpxi.xdlib.config.clothConfig.modConfigScreen;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 
 @Environment(EnvType.CLIENT)
 public class modMenuIntegration implements ModMenuApi {
-    public static final configManager CONFIG_MANAGER = new configManager();
     private static final boolean clothConfig = loader.isModLoaded("cloth-config");
 
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
         if (clothConfig) {
-            return parent -> new getModConfigScreen(CONFIG_MANAGER).getModConfigScreen(parent);
+            return parent -> new modConfigScreen().getModConfigScreen(parent);
         }
         return null;
     }
