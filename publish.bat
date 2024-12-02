bdd4ec8f37547aae6d03379166f7e16ebecdcb95
Ztrolix <xdpxigaming@gmail.com>
2024-12-02 20:30:08 +1100

    Removed Unused Batch Scripts

--- publish.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-title XD's Library - Publishing
-cls
-
-gradlew publish --warning-mode all
-pause
--- test.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-title XD's Library - Testing
-cls
-
-gradlew runClient
-pause
