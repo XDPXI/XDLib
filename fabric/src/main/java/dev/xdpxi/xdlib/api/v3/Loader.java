e82dfd8066df741abbb81d98dfc8e4a5410e2828
Ztrolix <xdpxigaming@gmail.com>
2024-12-28 16:47:37 +1100

    Ported LoaderAPI from FiberAPI to XDLib

---
+++ fabric/src/main/java/dev/xdpxi/xdlib/api/v3/Loader.java
@@ -0,0 +1,21 @@
+package dev.xdpxi.xdlib.api.v3;
+
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.ModContainer;
+import net.fabricmc.loader.api.metadata.ModMetadata;
+
+public class Loader {
+    public static boolean isModLoaded(String modID) {
+        return FabricLoader.getInstance().isModLoaded(modID);
+    }
+
+    public static String versionOfMod(String modID) {
+        FabricLoader loader = FabricLoader.getInstance();
+        ModContainer modContainer = loader.getModContainer(modID).orElse(null);
+        if (modContainer != null) {
+            ModMetadata metadata = modContainer.getMetadata();
+            return metadata.getVersion().getFriendlyString();
+        }
+        return null;
+    }
+}
