43319d82edf927daf8d887fda46a332259e9cdb4
Ztrolix <xdpxigaming@gmail.com>
2025-02-08 18:31:59 +1100

    Moved APIs to V3

--- src/client/java/dev/xdpxi/xdlib/api/v3/LangUtil.java
+++ src/client/java/dev/xdpxi/xdlib/api/v3/LangUtil.java
@@ -1,4 +1,4 @@
-package dev.xdpxi.xdlib.api;
+package dev.xdpxi.xdlib.api.v3;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.resource.language.LanguageManager;
--- src/main/java/dev/xdpxi/xdlib/api/v3/Register.java
+++ src/main/java/dev/xdpxi/xdlib/api/v3/Register.java
@@ -1,4 +1,4 @@
-package dev.xdpxi.xdlib.api;
+package dev.xdpxi.xdlib.api.v3;
 
 import dev.xdpxi.xdlib.Main;
 import dev.xdpxi.xdlib.util.Log;
