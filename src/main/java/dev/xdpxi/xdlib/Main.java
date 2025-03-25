8ea6f68b5371f37cd34b416c98d86823210a7086
Ztrolix <xdpxigaming@gmail.com>
2025-03-26 08:29:25 +1100

    Created Config for Main Mod

--- src/main/java/dev/xdpxi/xdlib/Main.java
+++ src/main/java/dev/xdpxi/xdlib/Main.java
@@ -1,6 +1,8 @@
 package dev.xdpxi.xdlib;
 
+import dev.xdpxi.xdlib.api.v5.Configuration;
 import dev.xdpxi.xdlib.api.v5.Register;
+import dev.xdpxi.xdlib.util.Config;
 import dev.xdpxi.xdlib.util.Log;
 import net.fabricmc.api.ModInitializer;
 
@@ -11,7 +13,13 @@ public class Main implements ModInitializer {
     public void onInitialize() {
         Log.info("[XDLib/Main] - Loading...");
 
+        Log.info("[XDLib/Main] - Loading configuration...");
+        Configuration.ConfigLoader.loadConfig(Config.class);
+
+        Log.info("[XDLib/Main] - Checking for Updates...");
         UpdateChecker.checkForUpdate();
+
+        Log.info("[XDLib/Main] - Initializing Register API...");
         Register.init();
 
         Log.info("[XDLib/Main] - Loaded!");
---
+++ src/main/java/dev/xdpxi/xdlib/util/Config.java
@@ -0,0 +1,8 @@
+package dev.xdpxi.xdlib.util;
+
+import dev.xdpxi.xdlib.api.v5.Configuration;
+
+@Configuration.Setup(name = "MyConfig", file = "config/mymod.json")
+public class Config implements Configuration.Config {
+
+}
