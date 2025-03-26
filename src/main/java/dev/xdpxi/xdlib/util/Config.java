8526f1c2688c3df093aebd43a1eed7a4b075f657
Ztrolix <xdpxigaming@gmail.com>
2025-03-26 19:04:49 +1100

    Updated Config Path

--- src/main/java/dev/xdpxi/xdlib/util/Config.java
+++ src/main/java/dev/xdpxi/xdlib/util/Config.java
@@ -2,7 +2,6 @@ package dev.xdpxi.xdlib.util;
 
 import dev.xdpxi.xdlib.api.v5.Configuration;
 
-@Configuration.Setup(name = "MyConfig", file = "config/mymod.json")
+@Configuration.Setup(name = "MyConfig", file = "config/xdlib/config.json")
 public class Config implements Configuration.Config {
-
 }
