8d1a1ad620747c861bcede4c691ab7e5005ad4a5
Ztrolix <xdpxigaming@gmail.com>
2025-03-26 19:05:05 +1100

    Fixed Config Name

--- src/main/java/dev/xdpxi/xdlib/util/Config.java
+++ src/main/java/dev/xdpxi/xdlib/util/Config.java
@@ -2,6 +2,6 @@ package dev.xdpxi.xdlib.util;
 
 import dev.xdpxi.xdlib.api.v5.Configuration;
 
-@Configuration.Setup(name = "MyConfig", file = "config/xdlib/config.json")
+@Configuration.Setup(name = "XD's Library", file = "config/xdlib/config.json")
 public class Config implements Configuration.Config {
 }
