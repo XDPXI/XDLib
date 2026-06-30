653c3fa4f1a43f297a2b2702054acda5832c1be6
Xavier Perri <contact@xdpxi.dev>
2026-06-30 21:28:56 +1000

    Update default version

--- common/src/main/java/dev/xdpxi/xdlib/Constants.java
+++ common/src/main/java/dev/xdpxi/xdlib/Constants.java
@@ -4,5 +4,5 @@ public class Constants {
 
     public static final String MOD_ID = "xdlib";
 
-    public static final String VERSION = "1.0.0-SNAPSHOT";
+    public static final String VERSION = "7.0.0-SNAPSHOT";
 }
